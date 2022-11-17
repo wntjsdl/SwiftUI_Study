c4a0c4a44aea9d3e88aa04ebea1cb7cbf35ceef5
Jusun Kang <wntjs0305@cocen.com>
2022-11-17 23:51:15 +0900

    SwiftUI BasicStudy Placeholder

---
+++ SwiftUI_Text_Placeholder/SwiftUI_Text_Placeholder/SwiftUI_Text_PlaceholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Text_PlaceholderApp.swift
+//  SwiftUI_Text_Placeholder
+//
+//  Created by powerapp on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Text_PlaceholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
