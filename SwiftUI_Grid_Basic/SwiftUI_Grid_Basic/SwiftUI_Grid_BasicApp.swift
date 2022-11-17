9fa49c2714d59a88d6d8d8db5aad327ca5c4f860
Jusun Kang <wntjs0305@cocen.com>
2022-11-17 23:33:25 +0900

    SwiftUI BasicStudy Grid

---
+++ SwiftUI_Grid_Basic/SwiftUI_Grid_Basic/SwiftUI_Grid_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Grid_BasicApp.swift
+//  SwiftUI_Grid_Basic
+//
+//  Created by powerapp on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Grid_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
